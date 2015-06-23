3355e956905aaefa8e43dea391b706eace3c6b37
suyhmedu <suyhmed@gmail.com>
2015-06-23 01:32:20 -0400

    Microphone added
    Added Microphone

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -15,9 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Show text that says recording in progress
+        //TODO: record user voice 
+        print("recordAudio")
     }
 
 
